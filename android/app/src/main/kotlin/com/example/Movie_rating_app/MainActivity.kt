5c59e95843d141a1df4d348465803d71d3d13b0c
Hcastro1515 <Elmenolx8@gmail.com>
2020-08-07 01:26:54 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/Movie_rating_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Movie_rating_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/movie.dart
@@ -0,0 +1,9 @@
+class Movie {
+  String movieName;
+  String movieDescription;
+  String movieImgURL;
+  int rating;
+
+  Movie({this.movieName, this.movieDescription, this.movieImgURL, this.rating});
+}
+
